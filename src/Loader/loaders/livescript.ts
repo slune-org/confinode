f3f65eab2693ec21c96682937c75896f6ffc69e5
Stéphane Veyret <stephane@neptura.org>
2020-02-22 13:33:41 +0100

    Add livescript loader

---
+++ src/Loader/loaders/livescript.ts
@@ -0,0 +1,11 @@
+import { LoaderDescription, RequiringLoader } from '../Loader'
+
+/**
+ * Loader description.
+ */
+const description: LoaderDescription = {
+  filetypes: 'ls',
+  Loader: RequiringLoader,
+  module: 'livescript',
+}
+export default description
