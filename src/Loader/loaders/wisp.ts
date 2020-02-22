16cc42c4311530b4ca12f2b8b2162275f3c0b101
Stéphane Veyret <stephane@neptura.org>
2020-02-22 13:58:12 +0100

    Add wisp loader

---
+++ src/Loader/loaders/wisp.ts
@@ -0,0 +1,11 @@
+import { LoaderDescription, RequiringLoader } from '../Loader'
+
+/**
+ * Loader description.
+ */
+const description: LoaderDescription = {
+  filetypes: 'wisp',
+  Loader: RequiringLoader,
+  module: 'wisp/engine/node',
+}
+export default description
